6dfbab7b33f0e20a24af70a65328358e21cc028e
softpaper95 <minho100227@likelion.org>
2020-10-25 21:14:48 +0900

    revise wear2

--- wear2/js/selected.js
+++ wear2/js/selected.js
@@ -5,10 +5,15 @@
 
 var photo1 = document.getElementById('selected_pd');
 var photo2 = document.getElementById('click1');
+var all_set = document.getElementById('click2');
 
 
 
 
 photo2.addEventListener('click', function() {
   photo1.style.opacity = "1";
+});
+
+all_set.addEventListener('click', function() {
+  photo1.style.opacity = "0";
 });
